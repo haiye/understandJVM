de40ef88d66ab28087a23666751c5a73331ca147
haiye <yehaimin16@gmail.com>
2017-02-13 22:55:29 +0800

    update

---
+++ javaProject/src/book/ch03/MainTest.java
@@ -0,0 +1,10 @@
+package book.ch03;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        for (int i=0; i<100000000;i ++){
+            System.out.println(i);
+        }
+    }
+}
